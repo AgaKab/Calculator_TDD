f9a064422a21717edd5e63dab6cd531389701d1d
AgaKab <agata,kabot@vp.pl>
2024-05-06 21:05:19 +0200

    def rename

--- calculator_test.py
+++ calculator_test.py
@@ -1,17 +1,20 @@
 import unittest
 from calculator import Calculator
-
+   
     
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
 
-    def add_test(self):
-        self.assertEqual(30, self.calculator.add(10,20))
+    def test_add(self):
+        self.assertEqual(self.calculator.add(10,20), 30)
         self.assertEqual(-177, self.calculator.add(-123,-54))
         self.assertEqual(-10, self.calculator.add(10,-20))
         self.assertEqual(-10, self.calculator.add(-10,0))
-
+        
+    def test_subtract(self):
+        self.assertEqual(10, self.calculator.subtract(20,10))
+        self.assertEqual(98, self.calculator.subtract(99,1))
 
 if __name__ == '__main__':
     unittest.main()
