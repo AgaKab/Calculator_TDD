0c4aa2faf9b61903cf1c752c6eb55647b22c76cc
AgaKab <agata,kabot@vp.pl>
2024-05-06 21:33:31 +0200

    divide test added

--- calculator_test.py
+++ calculator_test.py
@@ -15,6 +15,21 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(10, self.calculator.subtract(20,10))
         self.assertEqual(98, self.calculator.subtract(99,1))
-
+        
+    def test_multiply(self):
+        self.assertEqual(10, self.calculator.multiply(2,5))
+        self.assertEqual(-8, self.calculator.multiply(2,-4))
+        self.assertEqual(0, self.calculator.multiply(0,99))
+    
+    def test_divide(self):
+        self.assertEqual(5, self.calculator.divide(25,5))
+        self.assertEqual(-8, self.calculator.divide(32,-4))
+        self.assertEqual(0, self.calculator.divide(0,99))
+    
+    def test_divide_by_zero(self):
+        # self.assertEqual(self.calculator.divide(99,0), "Do not divide by zero!")
+        with self.assertRaises(ValueError):
+            self.calculator.divide(99, 0)
+    
 if __name__ == '__main__':
     unittest.main()
